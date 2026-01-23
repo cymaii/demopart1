a269962ff9e68729b3b6aed4fca2ed29bc3104c9
cymaii <maiichi0911@gmail.com>
2026-01-23 20:13:30 +0700

    Update PlayerShooting to support automatic shooting with interval

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    public GameObject bulletPrefabs;
+    public float shootingInterval;
+    private float lastBulletTime;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
-            Instantiate(bulletPrefab, transform.position, transform.rotation);
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                ShootBullet();
+                lastBulletTime = Time.time;
+            }
         }
+    }
+
+    private void ShootBullet()
+    {
+        Instantiate(bulletPrefabs, transform.position, transform.rotation);
     }
 }
 
